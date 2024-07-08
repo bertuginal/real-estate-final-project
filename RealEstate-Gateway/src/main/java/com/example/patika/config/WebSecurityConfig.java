6ba7902965be96e8de4f075b02e35eb504158060
bertug <bertuginal@gmail.com>
2024-07-09 00:15:25 +0300

    realestate-gateway has been added!

---
+++ RealEstate-Gateway/src/main/java/com/example/patika/config/WebSecurityConfig.java
@@ -0,0 +1,71 @@
+package com.example.patika.config;
+
+import com.example.patika.filter.JwtFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+
+@Configuration
+public class WebSecurityConfig {
+
+    @Bean
+    SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        // -- @formatter:off --
+        http
+                .httpBasic().disable()
+                .formLogin().disable()
+                .csrf().disable();
+
+        return http.build();
+        // -- @formatter:on --
+
+    }
+    @Autowired
+    private JwtFilter filter;
+
+    @Bean
+    public RouteLocator routes(RouteLocatorBuilder builder) {
+        return builder.routes()
+
+
+                //login işlemi yapılmadan; kullanıcı oluşturulabilir, login işlemi yapılabilir. filtreden geçmez.
+
+                .route("realestate-user",
+                        r -> r.method(HttpMethod.POST)
+                                .and()
+                                .path(("/individual-users/**"))
+                                .uri("lb://realestate-user"))
+                .route("realestate-auth",
+                        r -> r.method(HttpMethod.POST)
+                                .and()
+                                .path("/auth/**")
+                                .uri("lb://realestate-auth"))
+                .route("realestate-user",
+                        r -> r.method(HttpMethod.GET)
+                                .and()
+                                .path(("/individual-users/**"))
+                                .filters(f -> f.filter(filter)).uri("lb://realestate-user"))
+                .route("realestate-advert",
+                        r -> r.method(HttpMethod.GET,HttpMethod.POST,HttpMethod.PUT,HttpMethod.DELETE)
+                                .and()
+                                .path("/adverts/**")
+                                .filters(f -> f.filter(filter)).uri("lb://realestate-advert"))
+                .route("realestate-purchase",
+                        r -> r.method(HttpMethod.GET,HttpMethod.POST)
+                                .and()
+                                .path("/purchases/**")
+                                .filters(f -> f.filter(filter)).uri("lb://realestate-purchase"))
+                .route("realestate-payment",
+                        r -> r.path("/payments/**")
+                                .filters(f -> f.filter(filter)).uri("lb://realestate-payment"))
+                .build();
+
+    }
+
+}
---
+++ RealEstate-Gateway/src/main/java/com/example/patika/util/JwtUtil.java
@@ -0,0 +1,69 @@
+package com.example.patika.util;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.security.Key;
+import java.util.Date;
+import java.util.function.Function;
+
+@Component
+@Slf4j
+public class JwtUtil {
+
+	private static final String SECRET_KEY = "realestate-patika-realy-secret-key-realestate-patika-realy-secret-key";
+
+	private Key key;
+
+	@PostConstruct
+	public void init() {
+		this.key = Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+	}
+
+	public Claims getAllClaimsFromToken(String token) {
+
+		//// @formatter:off
+		return Jwts
+				.parserBuilder()
+				.setSigningKey(key)			
+				.build()
+				.parseClaimsJws(token)
+				.getBody();
+			// @formatter:on
+
+	}
+
+	public <T> T getClaimFromToken(String token, Function<Claims, T> claimsResolver) {
+		final Claims claims = getAllClaimsFromToken(token);
+		return claimsResolver.apply(claims);
+	}
+
+	public String getUserName(String token) {
+		return getClaimFromToken(token, Claims::getSubject);
+	}
+
+	public Date getExpirationDate(String token) {
+		return getClaimFromToken(token, Claims::getExpiration);
+	}
+
+	public boolean isTokenExpired(String token) {
+
+		return this.getExpirationDate(token).before(new Date());
+
+	}
+
+	public boolean isValidToken(String token) {
+		try {
+			return isTokenExpired(token);
+		} catch (Exception e) {
+			log.error("token is expired");
+		}
+
+		return true;
+	}
+
+}
